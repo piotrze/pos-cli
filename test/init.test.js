a7c9770b22bfdcf124bb65de288188ddf7192fbb
Paweł Kowalski <pavelloz@gmail.com>
2019-08-09 21:23:23 +0200

    Init tests

--- test/init.test.js
+++ test/init.test.js
@@ -1,47 +1,71 @@
+/* global afterAll, beforeAll */
+const glob = require('tiny-glob');
+const rimraf = require('rimraf');
+
 const exec = require('./utils/exec');
 const cliPath = require('./utils/cliPath');
 
-const glob = require('tiny-glob');
-
 const cwd = name => `${process.cwd()}/test/fixtures/init/${name}`;
+
+const expectedFiles = [
+  'app/translations/en.yml',
+  'app/views/partials/layout/footer.liquid',
+  'app/notifications/sms_notifications/example.liquid',
+  'app/authorization_policies/example_policy.liquid',
+  'app/graph_queries/example.graphql',
+  'app/notifications/api_call_notifications/example.liquid',
+  'app/notifications/email_notifications/example.liquid'
+];
 
 test('Does not override default directory contents without force flag', async () => {
   try {
     await exec(`${cliPath} init`, { cwd: cwd('notEmpty') });
-  } catch(e) {
-    const files = await glob('**', { cwd: cwd('notEmpty'), filesOnly: true });
-    expect(files.length).toBe(0);
-    expect(`${e}`).toMatch('Init failed. Reason: destination directory is not empty, aborting. Use options.force to override');
+  } catch (e) {
+    const files = await glob('**', { cwd: cwd('notEmpty'), filesOnly: true, dot: true });
+    expect(files.length).toBe(1);
+    expect(`${e}`).toMatch(
+      'Init failed. Reason: destination directory is not empty, aborting. Use options.force to override'
+    );
   }
 });
 
-test('Default directory structure is created', async () => {
+test('Default directory structure is created, --force is working', async () => {
   const { stdout } = await exec(`${cliPath} init --force`, { cwd: cwd('default') });
+  const files = await glob('**', { cwd: cwd('default'), filesOnly: true });
+
 
   expect(stdout).toMatch('Directory structure sucessfully created.');
 
-  const files = await glob('**', { cwd: cwd('default'), filesOnly: true });
-  expect(files).toContain('app/translations/en.yml');
-  expect(files).toContain('app/views/partials/layout/footer.liquid');
-  expect(files).toContain('app/notifications/sms_notifications/example.liquid');
-  expect(files).toContain('app/authorization_policies/example_policy.liquid');
-  expect(files).toContain('app/graph_queries/example.graphql');
-  expect(files).toContain('app/notifications/api_call_notifications/example.liquid');
-  expect(files).toContain('app/notifications/email_notifications/example.liquid');
+  expectedFiles.forEach(expected => {
+    expect(files).toContain(expected);
+  });
 });
 
-test.only('Inits with custom url and branch', async () => {
-  // const { stdout } = await exec(`${cliPath} init --force`, { cwd: cwd('custom') });
+test('Inits with custom url and branch', async () => {
+  const url = 'git@bitbucket.org:pavelloz/directory-structure.git';
+  const branch = 'pos-cli-test';
 
-  // expect(stdout).toMatch('Directory structure sucessfully created.');
+  const { stdout } = await exec(`${cliPath} init --url ${url} --branch ${branch} --force`, { cwd: cwd('custom') });
+  const files = await glob('**', { cwd: cwd('custom'), filesOnly: true });
 
-  // const files = await glob('**', { cwd: cwd('default'), filesOnly: true });
-  // expect(files).toContain('app/translations/en.yml');
-  // expect(files).toContain('app/views/partials/layout/footer.liquid');
-  // expect(files).toContain('app/notifications/sms_notifications/example.liquid');
-  // expect(files).toContain('app/authorization_policies/example_policy.liquid');
-  // expect(files).toContain('app/graph_queries/example.graphql');
-  // expect(files).toContain('app/notifications/api_call_notifications/example.liquid');
-  // expect(files).toContain('app/notifications/email_notifications/example.liquid');
 
+  expect(stdout).toMatch('Directory structure sucessfully created.');
+
+  expectedFiles.forEach(expected => {
+    expect(files).toContain(expected);
+  });
 });
+
+const cleanup = () => {
+  const opts = {
+    glob: {
+      dot: true
+    }
+  };
+
+  rimraf.sync(`${cwd('custom')}/*`, opts);
+  rimraf.sync(`${cwd('default')}/*`, opts);
+};
+
+afterAll(cleanup);
+beforeAll(cleanup);
